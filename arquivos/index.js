becc5344bf258eef9a541d06bd551f5a97b90f33
4llay <4llayjpcg@gmail.com>
2023-06-12 19:09:42 -0300

    js bug fix

--- arquivos/index.js
+++ arquivos/index.js
@@ -1,46 +1,32 @@
-/*
-class Produto{
-    
-    constructor(nome, descrição, imgSource, preço){
-        this.nome = nome;
-        this.descrição = descrição;
-        this.imgSource = imgSource;
-        this.preço = preço;
-    }
-
-    teste(){
-        console.log(`${this.nome}\n${this.descrição}\n${this.imgSource}\n${this.preço}`);
-    }
-}
-
-const produto1 = new Produto("trufa de amendoim", "tem amendoim", "blablabla.png","69,420");
-
-produto1.teste();
-*/
-
 const track = document.getElementById("produtos");
 const images = track.getElementsByClassName("produto");
+var porcentagem = -1;
 
-window.onmousedown = e => {
+produtos.onmousedown = e => {
     track.dataset.mouseApertadoEm = e.clientX;
 }
 
 produtos.onmousemove = e => {
-
+    
     if(track.dataset.mouseApertadoEm === "0") return;
     
-
     const mouseDelta = parseFloat(track.dataset.mouseApertadoEm) - e.clientX,
     maxDelta = window.innerWidth * 1.5;
     
     
-    const porcentagem = (mouseDelta / maxDelta) * -100;
+    porcentagem = (mouseDelta / maxDelta) * -100;
+    
     proxPorcentagem = parseFloat(track.dataset.velhaPorcentagem) + porcentagem;
-
-    console.log(parseFloat(track.dataset.velhaPorcentagem));
-
-    proxPorcentagem = Math.min( proxPorcentagem, 0);
+    
+    if(isNaN(track.dataset.velhaPorcentagem)){
+        track.dataset.velhaPorcentagem = porcentagem;
+        console.log("a");
+        return;
+    }
+    
+    proxPorcentagem = Math.min(proxPorcentagem, 0);
     proxPorcentagem = Math.max(proxPorcentagem, -91);
+    
 
     track.dataset.porcentagem = proxPorcentagem;
 
@@ -57,6 +43,7 @@ produtos.onmousemove = e => {
 }
 
 window.onmouseup = e => {
+
     track.dataset.mouseApertadoEm = "0";
     track.dataset.velhaPorcentagem = track.dataset.porcentagem;
 }
